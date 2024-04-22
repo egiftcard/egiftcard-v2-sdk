3c4a28bcc174b952b3cc753bb0de070630d3070f
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-04-22 18:52:41 +0000

    init commit

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,7 @@ export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
 
 export const FACTORY_ADDRESS_MAP: { [chainId: number]: string } = V2_FACTORY_ADDRESSES
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0x0f995f2ca5ec1013c2611e9cc5e09e2cfd3aa1f47685d2a88cc59b5247449b25'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
